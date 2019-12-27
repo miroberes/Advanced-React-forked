f6e4b1778c0e973a860e6670481279912dab628f
mberes <miro.beres@gmail.com>
2019-12-27 18:37:15 +0100

    added apollo client

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,15 +1,30 @@
 import { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
-function MyApp({ Component, pageProps }) {
-    console.log('pageProps', {...pageProps});
+function MyApp(props) {
+    // console.log('props', props);
     return (
         <Container>
-            <Page>
-                <Component {...pageProps} />
-            </Page>
+            <ApolloProvider client={props.apollo}>
+                <Page>
+                    <props.Component {...props.pageProps} /> {/* props. is just for clarity */}
+                </Page>
+            </ApolloProvider>
         </Container>
     );
 }
+// every page in the app will be server-side rendered because data has to be fetched before rendering
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+    let pageProps = {};
+    console.log('this runs first, before the App is rendered');
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+};
 
-export default MyApp;
+export default withData(MyApp);
